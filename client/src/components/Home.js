80be4615f32b19cad442939d3f896191c9b15706
Alfonso Graziano <alfonso.lokk3d@gmail.com>
2019-12-24 13:03:31 +0100

    aggiungo how it works

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,6 +6,11 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ImportExportIcon from '@material-ui/icons/ImportExport';
@@ -79,6 +84,25 @@ function Home() {
         <Button variant="contained" color="primary" 
         style={{padding: 10, width:300, marginTop:20}}
         onClick={()=>{window.location ="/singup"}}>crea un account</Button>
+
+        <ExpansionPanel style={{marginTop:50, maxWidth: 500, marginBottom: 50}}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>How it works?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Questo tool utile per i chitarristi principianti consente di tenere in cloud tutte le proprie canzoni preferite
+              Una volta aggiunta la canzone si può cambiare la tonalità degli accordi
+              Gli accordi vengono trovati automaticamente da un algoritmo che riconosce quali righe di testo contengono note e quali testo
+              Attenzione quindi ad aggiungere solo canzoni che hanno le note su righe separate rispetto al testo, diversamente potrebbe non funzionare
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        
       </div>
      
     </div>
